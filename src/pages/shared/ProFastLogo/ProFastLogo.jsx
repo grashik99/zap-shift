7fc9dd245454d370a0e86452a0580bf0934bb330
grashik99 <grashik99@gmail.com>
2025-07-31 02:14:04 +0300

    Adding  carousel in banner section

---
+++ src/pages/shared/ProFastLogo/ProFastLogo.jsx
@@ -0,0 +1,11 @@
+import logo from "../../../assets/logo.png";
+
+const ProFastLogo = () => {
+  return (
+    <div className="flex items-end">
+      <img className="mb-2" src={logo} alt="ProFast" />
+      <p className="-ml-2 text-3xl font-extrabold">ProFast</p>
+    </div>
+  );
+};
+export default ProFastLogo;
